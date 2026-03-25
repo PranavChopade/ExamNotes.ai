2b2ca410fcb0a028274f472b4193423ea246c324
Pranav Chopade <pranavchopade129@gmail.com>
2026-03-25 11:56:45 +0530

    add frontend url

--- server/src/app.js
+++ server/src/app.js
@@ -4,11 +4,11 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import notesRouter from "./routes/notes.routes.js";
-
+import { ENV } from "./config/ENV.js";
 // CORS configuration
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ENV.ORIGIN || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
--- server/src/config/ENV.js
+++ server/src/config/ENV.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 dotenv.config();
 
 export const ENV = {
+  ORIGIN: process.env.ORIGIN,
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
